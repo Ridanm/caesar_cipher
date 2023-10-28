faadd92340972d90c8c59a15898a8a2660f4cbab
Ridanm <danyfox1.dm@gmail.com>
2023-10-28 16:50:24 -0300

    caesar_cipher method added

---
+++ lib/caesar_cipher.rb
@@ -0,0 +1,43 @@
+class Code
+  attr_accessor :text, :range 
+
+  def initialize
+    @text = ''
+    @range = 0
+    @ab_z = [*'a'..'z']
+  end
+
+  def caesar_cipher(text, range)
+    @text = text
+    pass = ''
+
+    text.downcase.each_char do |char|
+      if verify_char(char)
+        pass << change_char(char, range)
+      else
+        pass << char
+      end
+    end
+  
+    pass
+  end
+
+  def verify_char(char)
+    @ab_z.include?(char.downcase) 
+  end
+
+  def give_me_back_char(ind)
+    @ab_z[ind]
+  end
+
+  def change_char(char, range)
+    new_range = (@ab_z.index(char) + range) % 26
+    give_me_back_char(new_range)
+  end
+
+  def decipher
+    @text
+  end 
+
+  private :change_char, :give_me_back_char, :verify_char
+end
---
+++ lib/main.rb
@@ -0,0 +1,19 @@
+require './caesar_cipher.rb'
+
+puts "This is a implementation of Caesar Cipher(Adz, 1)"
+
+phrase = Code.new 
+puts phrase.caesar_cipher('Adz', 1)
+
+puts "\nEnter the text you want to encrypt"
+phrase = gets.chomp
+
+puts "\nEnter the displacement number"
+key = gets.chomp.to_i
+
+puts "\nCipher text"
+encrypting = Code.new
+
+puts "Cipher: #{encrypting.caesar_cipher(phrase, key)}"
+
+puts "Decipher: #{encrypting.decipher}"
