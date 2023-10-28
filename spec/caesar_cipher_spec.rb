ad6caa6cc99be302b68d6a01812ad5799c626335
Ridanm <danyfox1.dm@gmail.com>
2023-10-28 17:28:22 -0300

    spec checking negative displacements

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -3,14 +3,19 @@ require '../lib/caesar_cipher.rb'
 RSpec.describe Code do
   context "checking text offset" do
     code = Code.new
-    it "is correct" do
+    it "moving 1 position." do
       expect(code.caesar_cipher('ab', 1)).to eq('bc')
     end
 
-    it "z to a" do
+    it "when should you go back to the beginning of the alphabet? z to a" do
       expect(code.caesar_cipher('az', 2)).to eq('cb')
     end
+
+    it "when the displacement is negative?" do
+      expect(code.caesar_cipher('az',-1)).to eq('zy')
+    end
   end
+
   context "check the original text" do
     code = Code.new
     it "entered text" do
