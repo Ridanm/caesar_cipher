7366e5e45302fa3d2f3999d9424b1189bcb47b05
Ridanm <danyfox1.dm@gmail.com>
2024-12-26 18:27:02 -0300

    Show encrypted string

--- lib/main.rb
+++ lib/main.rb
@@ -5,11 +5,11 @@ require './caesar_cipher'
 require './description'
 
 puts Presentation.show('title').colorize(:green)
-puts Presentation.show('enter_number')
+print Presentation.show('enter_number')
 key = gets.chomp.to_i
 
-puts Presentation.show('text')
+print Presentation.show('text')
 phrase = gets.chomp.to_s
 
-encrypt = Code.new(phrase, key)
-encrypt.caesar_cipher
+phrase_key = Code.new(phrase, key)
+puts Presentation.show('encryption'), phrase_key.caesar_cipher
