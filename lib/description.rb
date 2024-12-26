3e164747938b0ecc2812a03ff989dae8a43c5181
Ridanm <danyfox1.dm@gmail.com>
2024-12-26 10:58:10 -0300

    separating text from main file

---
+++ lib/description.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# This module contains the presentation information for the Cesar class.
+module Presentation
+  def self.show(parameter)
+    {
+      'title' => '  This is a implementation of Caesar Cipher',
+      'enter_number' => "\nEnter the number you want to offset in the alphabet.",
+      'text' => 'Enter the text you want to encrypt)'
+    }[parameter]
+  end
+end
