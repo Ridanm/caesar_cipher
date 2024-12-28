25bd411766aaf85bb05fd701f24631873b2ca5fd
Ridanm <danyfox1.dm@gmail.com>
2024-12-28 20:14:12 -0300

    Changing input order

--- lib/main.rb
+++ lib/main.rb
@@ -5,11 +5,13 @@ require './caesar_cipher'
 require './description'
 
 puts Presentation.show('title').colorize(:green)
-print Presentation.show('enter_number')
-key = gets.chomp.to_i
 
 print Presentation.show('text')
 phrase = gets.chomp.to_s
 
+print Presentation.show('enter_number')
+key = gets.chomp.to_i
+
 phrase_key = Code.new(phrase, key)
+
 print Presentation.show('encryption'), phrase_key.caesar_cipher, "\n"
